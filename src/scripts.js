4954fc89bf5e4f24c63b167e90d7a4f5f27a13ca
Emalee Poellot <emalee.poellot@gmail.com>
2023-11-07 20:26:48 -0700

    feat/Add event listener for when the submit button is clicked

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,13 @@
+//Imports Here:
 import { allDestinations, allTravelers, allTrips, fetchTravelerInfo } from './apiCalls';
+import { userLogsIn, findCurrentId } from './data-model';
 import './css/styles.css';
 
+//QuerySelectors Here:
+const submitButton = document.querySelector('#submit');
+
+//Dom Update Variables Here:
 const allTravelersData = fetchTravelerInfo(allTravelers);
-// const currentTraveler;
 const allTripsData = fetchTravelerInfo(allTrips);
 const allDestinationsData = fetchTravelerInfo(allDestinations);
 
@@ -10,11 +15,8 @@ console.log(allTripsData, 'all trips data')
 console.log(allTravelersData, 'all travelers data');
 console.log(allDestinationsData, 'destinations data');
 
-
-//Create queryselectors for username and password inputs. When the submit button is clicked, the page will open for them to see their data
-//
-//trips data is an array of objects: { id: 1, userID: 44, destinationID: 49, travelers: 1, date: '2022/09/16'}
-//traveler data is an array of 50 traveler objects: { id: 1, name: 'Hame Leadbeater', travelerType: 'relaxer'} 
-//destination data: {id: 1, destination: 'Lima, Peru', estimatedLodgingCostPerDay: 70, estimatedFlightCostPerPerson: 400, image: 'url'}
-
-// const getCurrentTraveler = 
+//Even Listeners Here:
+submitButton.addEventListener('click', function() {
+    // event.preventDefault()
+    findCurrentId();
+})
