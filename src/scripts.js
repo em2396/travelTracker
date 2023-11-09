fd26936b56903368953b92c297fe721db058c4d0
Emalee Poellot <emalee.poellot@gmail.com>
2023-11-08 17:35:23 -0700

    feat/Refactor fetch function invocation and assignment variables

--- src/scripts.js
+++ src/scripts.js
@@ -2,22 +2,21 @@
 import { allDestinations, allTravelers, allTrips, fetchTravelerInfo, urls } from './apiCalls';
 import { userLogsIn, findCurrentId } from './data-model';
 import './css/styles.css';
+import { displayCurrentTraveler } from './domUppdates';
 
 //QuerySelectors Here:
 const submitButton = document.querySelector('#submit');
-export let allTravelersData 
-//Dom Update Variables Here:
+export let allTravelersData; 
+export let currentTraveler;
 
 //Event Listeners Here:
 window.addEventListener('load', function() {
-    // console.log('hi')
-    Promise.all([fetchTravelerInfo(allTravelers)]).then(values => {
-        console.log(values)
+    Promise.all(fetchTravelerInfo).then(values => {
+        allTravelersData = values[0].travelers;
     })
 })
 
 submitButton.addEventListener('click', function(event) {
-    console.log(allTravelersData, 'inside button')
     event.preventDefault();
     findCurrentId();
 })
