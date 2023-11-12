f6562775a0c3915222f16d2949a1c687c87e054a
Emalee Poellot <emalee.poellot@gmail.com>
2023-11-12 15:04:37 -0700

    feat/Install datepicker and create function to add dates

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@
 import { fetchTravelerInfo } from './apiCalls';
 import { findCurrentId, upcoming, past, pending } from './data-model';
 import './css/styles.css';
-import { displayPast, displayUpcoming, displayPending } from './domUppdates';
+import { displayPast, displayUpcoming, displayPending, displayNewTripWindow } from './domUppdates';
 //clean up ^^^
 
 //QuerySelectors Here:
@@ -10,20 +10,33 @@ const submitButton = document.querySelector('#submit');
 const upcomingButton = document.querySelector('#upcomingButton');
 const pendingButton = document.querySelector('#pendingButton');
 const pastButton = document.querySelector('#pastButton');
+const submitTrip = document.querySelector('#submitTrip');
+const estimatedCostButton = document.querySelector('#estimatedCostButton');
+
+
+//Variable Here;
 export let allTravelersData; 
 export let allTrips;
 export let currentTraveler;
 export let allDestinations;
 
 //Event Listeners Here:
-window.addEventListener('load', function() {
+window.addEventListener('DOMContentLoaded', function() {
     Promise.all(fetchTravelerInfo).then(values => {
         console.log(values, 'inside promise');
         allTravelersData = values[0].travelers;
         allTrips = values[1].trips;
         allDestinations = values[2].destinations;
-    })
-})
+
+        const picker = datepicker(dateInput, {
+            onSelect: (instance, date) => {
+              const formattedDate = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`
+              dateInput.value = formattedDate
+            },
+            startDate: new Date (2023, 12, 12),
+          });
+    });
+});
 
 submitButton.addEventListener('click', function(event) {
     event.preventDefault();
@@ -40,5 +53,13 @@ pendingButton.addEventListener('click', function() {
 
 pastButton.addEventListener('click', function() {
     displayPast(past)
+});
+
+submitTrip.addEventListener('click', function() {
+
+});
+
+estimatedCostButton.addEventListener('click', function() {
+    estimateNewTripCost()
 });
 
