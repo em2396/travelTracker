f526442218896acd195fa080c826ea7bedadb242
Emalee Poellot <emalee.poellot@gmail.com>
2023-11-12 21:49:28 -0700

    feat/Create working object to post

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -6,7 +6,7 @@
 // export const modifySingleTrip = 'http://localhost:3001/api/v1/updateTrip';
 
 import { currentTraveler } from "./data-model"
-import { formattedDate } from "./scripts"
+import { formattedDate, allDestinations, allTrips} from "./scripts"
 
 export const urls = ['http://localhost:3001/api/v1/travelers','http://localhost:3001/api/v1/trips', 'http://localhost:3001/api/v1/destinations']
 
@@ -21,23 +21,52 @@ export const fetchTravelerInfo = urls.map(url => {
         .then((data) => {
             return data;
         })
-})
+});
 
+export const sendNewTrip = tripInfo => {
+   return fetch ('http://localhost:3001/api/v1/trips', {
+        method: 'POST',
+        body: JSON.stringify(tripInfo),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error (`${response.status}: Failed to fetch data`)
+        }
+        // console.log(response);
+       return response.json()
+    })
+    .then (json => {
+        // console.log(json, 'json')
+        allTrips.push(json);
+        // console.log(allTrips);
+
+    })
+}
+
+//duration and travelers neeed to benumbers
 export const submitNewTrip = () => {
-    console.log(formattedDate)
+    const destInput = allDestinations.find(element => {
+       return element.destination === chooseDestination.value
+    });
+    const destID = destInput.id;
     const dateRegex = /^\d{4}\/\d{2}\/\d{2}$/;
-    console.log(typeof numOfTravelers.value, 'numtraav')
-    console.log(dateRegex.test(dateInput.value), 'regex')
-    if (dateRegex.test(dateInput.value) && durationInput.value <= 30) {
+    const duration = parseInt(durationInput.value);
+    const numOfPeople = parseInt(numOfTravelers.value);
+    if (dateRegex.test(dateInput.value) && durationInput.value <= 30 && typeof numOfPeople === 'number') {
         const trip = {
+            id: allTrips.length + 1,
+            userID: currentTraveler.id,
+            destinationID: destID,
+            travelers: numOfPeople,
             date: dateInput.value,
-            destinationID: 'blank',
-            duration: durationInput.value,
-            id: 'blank for now',
-            status: 'pending',
-            travelers: numOfTravelers.value,
-            userID: currentTraveler.id
+            duration: duration,
+            status: 'pending'
         }
         console.log(trip, 'trip')
+        sendNewTrip(trip);
+        return trip;
     }
 }
